3c837a05748f7568c0f4288162409f3cde750edf
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-15 17:07:34 +0700

    Update leaderboard.service.ts

--- api/src/leaderboard/leaderboard.service.ts
+++ api/src/leaderboard/leaderboard.service.ts
@@ -1,1 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+@Injectable()
+export class LeaderboardService {
+  constructor(private prisma: PrismaService) {}
+  async getTop5() {
+    return this.prisma.leader.findMany({ orderBy: { score: 'desc' }, take: 5 });
+  }
+}
 
